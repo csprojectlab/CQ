57181248782732b59bd2adef5525f4bab1b578ee
HP <HP@LAPTOP-PEQ55ORK>
2018-11-01 14:40:03 +0530

    Created character printing method

--- AACoding/src/util/Util.java
+++ AACoding/src/util/Util.java
@@ -12,6 +12,14 @@ public class Util {
 		System.out.println();
 	}
 	
+	public static void printMatrix(char[][] m) {
+		for(int i = 0; i < m.length; i++) {
+			for(int j = 0; j < m[i].length; j++)
+				System.out.print(m[i][j] + " ");
+			System.out.println();
+		}
+	}
+	
 	public static void printMatrix(boolean[][] m) {
 		for(int i = 0; i < m.length; i++) {
 			for(int j = 0; j < m[i].length; j++) {
